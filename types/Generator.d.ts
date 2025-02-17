bfec3a888dae0b30b8724be5a755a23a322a9d9f
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-02-17 01:15:45 -0500

    [ISSUE-1] Adding json data for the generator

---
+++ types/Generator.d.ts
@@ -0,0 +1,15 @@
+export type GeneratorItem = {
+  name: string;
+  type: string;
+  rarity: string;
+  cost?: number;
+};
+
+export type Weapon = {
+  name: string;
+  type: string;
+  rarity: string;
+  cost?: number;
+  no_attach?: boolean;
+  no_attach_info?: boolean;
+};
