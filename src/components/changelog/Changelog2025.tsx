58f2fe7a604d42425e94600398536956f7cd0b24
Bana0615 <aelbaneh0615@gmail.com>
2025-06-13 11:01:20 -0400

    Updated styling of changelog page

--- src/components/changelog/Changelog2025.tsx
+++ src/components/changelog/Changelog2025.tsx
@@ -4,25 +4,32 @@ import { Row, Col } from 'react-bootstrap';
 import Link from 'next/link';
 // --- Data ---
 import data from '@/data/changelog/2025.json';
+// --- Styling ---
+import styles from './Changelog.module.css';
 
 function Changelog2025() {
   return (
     <>
-      <Row>
-        <Col>
-          {data.map((entry, index) => (
-            <p key={index}>
-              <strong>{entry.Date}:</strong>{' '}
-              {entry.Link && entry.Link.Url && entry.Link.Text && (
-                <>
-                  <Link href={entry.Link.Url}>{entry.Link.Text}:</Link>{' '}
-                </>
-              )}
-              {entry.Changes}
-            </p>
-          ))}
-        </Col>
-      </Row>
+      <div className={styles.timeline}>
+        {data.map((entry, index) => (
+          <div
+            key={index}
+            className={`${styles.timelineItem} ${styles[entry.Type?.toLowerCase()]}`}>
+            <div className={styles.timelineContent}>
+              <p className={styles.date}>{entry.Date}</p>
+              <h5>{entry.Type}</h5>
+              <p>
+                {entry.Link?.Url && entry.Link?.Text && (
+                  <>
+                    <Link href={entry.Link.Url}>{entry.Link.Text}</Link>{' '}
+                  </>
+                )}
+                {entry.Changes}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
